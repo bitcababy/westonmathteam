f61a6a12f93747ed909af66457e6f68b79b45acc
Meredith Lesly <bitcababy+github@gmail.com>
2011-07-28 09:07:13 -0400

    Added problmes as a resource

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,8 @@ Westonmathteam::Application.routes.draw do
   # Sample resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
 
+	resources :problems
+
   # Sample resource route with options:
   #   resources :products do
   #     member do
