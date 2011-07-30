69d2b5ae70f73446de2e5f8cf1fa2d3c6e1b3579
Meredith Lesly <bitcababy+github@gmail.com>
2011-07-30 12:09:39 -0400

    Uncommented teh preface (may not be necessary)

--- features/step_definitions/web_steps.rb
+++ features/step_definitions/web_steps.rb
@@ -18,19 +18,19 @@
 # * http://elabs.se/blog/15-you-re-cuking-it-wrong
 #
 
-# 
-# require 'uri'
-# require 'cgi'
-# require File.expand_path(File.join(File.dirname(__FILE__), "..", "support", "paths"))
-# require File.expand_path(File.join(File.dirname(__FILE__), "..", "support", "selectors"))
-# 
-# module WithinHelpers
-#   def with_scope(locator)
-#     locator ? within(*selector_for(locator)) { yield } : yield
-#   end
-# end
-# World(WithinHelpers)
-# 
+
+require 'uri'
+require 'cgi'
+require File.expand_path(File.join(File.dirname(__FILE__), "..", "support", "paths"))
+require File.expand_path(File.join(File.dirname(__FILE__), "..", "support", "selectors"))
+
+module WithinHelpers
+  def with_scope(locator)
+    locator ? within(*selector_for(locator)) { yield } : yield
+  end
+end
+World(WithinHelpers)
+
 # # Single-line step scoper
 # When /^(.*) within (.*[^:])$/ do |step, parent|
 #   with_scope(parent) { When step }
