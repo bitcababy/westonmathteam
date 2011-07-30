bd21e68cb148063953bff2cb3f85cad3e98bc9df
Meredith Lesly <bitcababy+github@gmail.com>
2011-07-30 12:36:14 -0400

    Everything is covered by Cucumber now

--- spec/views/index.html.haml_spec.rb
+++ spec/views/index.html.haml_spec.rb
@@ -6,27 +6,5 @@ describe 'problems/index.html.haml' do
 	before :each do
 		@problems = make_some_mock_problems
 	end
-	
-	#! This is all covered by Cucumber now
-	# context "the problem table" do
-	# 	it "shows headers" do
-	# 		assign(:problems, [])
-	# 		col_heads = %W(Problem Solution Year Meet Round Number)
-	# 		render
-	# 		rendered.should have_selector('table') do
-	# 			rendered.should have_selector('thead') do
-	# 				rendered.should have_selector('tr') do
-	# 					col_heads.each do |head|
-	# 						rendered.should have_selector('td') do
-	# 							rendered.should contain(head)
-	# 						end
-	# 					end
-	# 				end
-	# 			end
-	# 		end # shows the headers
-	# 	end
-	# 		
-	# 		
-	# end # the problem table
 		
 end
