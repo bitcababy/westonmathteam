53e65c3e2080583b96d8d08e401a929fbedc469f
Meredith Lesly <bitcababy+github@gmail.com>
2011-07-30 12:21:47 -0400

    Added #make_some_factory_problems

--- spec/support/problems_helper.rb
+++ spec/support/problems_helper.rb
@@ -2,4 +2,9 @@ module ProblemHelper
 	def make_some_mock_problems(n=10)
 		(1..n).collect { mock_model('Problem') }
 	end
+	
+	def make_some_factory_problems(n=10)
+		(1..n).collect { Factory(:problem) }
+	end
+	
 end
