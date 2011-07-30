aebffec83e13f9ccf637236fccb97b4c3467547f
Meredith Lesly <bitcababy+github@gmail.com>
2011-07-30 12:09:05 -0400

    Added steps to filter results and check if there's a way to show how many entries.

--- features/step_definitions/datatables_steps.rb
+++ features/step_definitions/datatables_steps.rb
@@ -3,6 +3,11 @@ When /^I click on the Year heading$/ do
 	page.execute_script(script)
 end
 
+When /^I enter "([^"]*)" in the search box$/ do |arg1|
+	script = "$('#problems').dataTable().fnFilter('#{arg1}')"
+	page.execute_script(script)
+end
+
 Then /^I should see the following sortable headings$/ do |table|
 	table.hashes.each do |hash|
 		heading = hash['heading']
@@ -10,4 +15,12 @@ Then /^I should see the following sortable headings$/ do |table|
 		selector2 = "#{selector} DataTables_sort_wrapper"
 		page.has_css?(selector2)
 	end
+end
+
+Then /^there should be a way to choose how many entries to show$/ do
+	page.has_css?("#problems_length")
+end
+
+Then /^there should be a way to search for specific problems$/ do
+	page.has_css?("#problems_filter")
 end
