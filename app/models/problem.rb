27423eeb0a8ad8b94c37bbd941f3ee1011f0a6ba
Meredith Lesly <bitcababy+github@gmail.com>
2011-07-30 11:49:39 -0400

    Added code to return only problems without images

--- app/models/problem.rb
+++ app/models/problem.rb
@@ -1,2 +1,10 @@
 class Problem < ActiveRecord::Base
+	def has_img_tag?
+		self.problem.include?('<img')
+	end
+
+	def self.problems_wo_images
+	    self.all.reject {|p| p.has_img_tag? }
+	end
+	
 end
